de52dd82e3596db0f1f8c6c761b3383b116b372f
MaxielDJ7 <mid6>
2016-02-10 19:18:40 -0500

    Credentials updated

--- OCI/dbcreds.php
+++ OCI/dbcreds.php
@@ -1,9 +1,9 @@
 <?php
 
-$hostname  =   " "  ;
-$username  =   " "	;
-$password  =   " "	;
-$project   =   " "	;
+$hostname  =   " sql1.njit.edu"  ;
+$username  =   " lfm6"	;
+$password  =   "15incJb4 "	;
+$project   =   "lfm6 "	;
 
 print "Account OK Syntactically <br>" ; 
 ?>
