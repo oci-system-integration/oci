73c8db35e76cc0b7e2c6c3c8b4aa9c5a9a006203
MaxielDJ7 <mid6>
2016-02-14 20:44:01 -0500

    Successful Connection

--- OCI/account.php
+++ OCI/account.php
@@ -4,8 +4,6 @@
 
 // connects to the databases
 
-
-
 include (  "dbcreds.php"     ) ;
 ( $dbh = mysql_connect( $hostname, $username, $password ) )
 	       	or die ( "Unable to connect to MySQL database: " . mysql_error());
@@ -13,21 +11,7 @@ print "Connected to MySQL<br>";
 mysql_select_db( $project );
 
 
-/*
-// Create connection
-$conn = new mysqli($hostname, $username, $password);
 
-// Check connection
-if (mysqli_connect_error()) {
-    die("Database connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-
-//select db
-
-$mysqli->select_db($project);
-
-*/
 //define variables
 
 
