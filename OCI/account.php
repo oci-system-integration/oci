6264016c2092a1e901f55e08eff6cbe9f316557e
MaxielDJ7 <mid6>
2016-02-14 20:23:12 -0500

    Attempting connection
    Commented mysqli equivalent

--- OCI/account.php
+++ OCI/account.php
@@ -4,18 +4,35 @@
 
 // connects to the databases
 
+
+
 include (  "dbcreds.php"     ) ;
-( $dbh = mysql_connect ( $hostname, $username, $password ) )
-	       	or die ( "Unable to connect to MySQL database" );
+( $dbh = mysql_connect( $hostname, $username, $password ) )
+	       	or die ( "Unable to connect to MySQL database: " . mysql_error());
 print "Connected to MySQL<br>";
-mysql_select_db( $project ); 
+mysql_select_db( $project );
 
 
+/*
+// Create connection
+$conn = new mysqli($hostname, $username, $password);
+
+// Check connection
+if (mysqli_connect_error()) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+//select db
+
+$mysqli->select_db($project);
+
+*/
 //define variables
 
 
 //if new account: save input into database & create session ids
-		
+
 		//encrypt password when saving it into db
 
 
