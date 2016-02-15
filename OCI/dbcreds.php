6897e1e84a9de28bd9a05936a3fc71b8a7b1405c
MaxielDJ7 <mid6>
2016-02-14 20:22:05 -0500

    Clean up

--- OCI/dbcreds.php
+++ OCI/dbcreds.php
@@ -2,8 +2,8 @@
 
 $hostname  =   "sql1.njit.edu"  ;
 $username  =   "lfm6"	;
-$password  =   "15incJb4 "	;
+$password  =   "15incJb4"	;
 $project   =   "lfm6"	;
 
-print "Account OK Syntactically <br>" ; 
+print "Account OK Syntactically <br>" ;
 ?>
